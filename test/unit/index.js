d9ffba371d48a286f314f9e7627168b0ed339862
antoinehage <me@antoinehage.com>
2019-08-29 09:57:38 -0400

    added test skeleton

---
+++ test/unit/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+describe("Starting Unit test", () => {
+
+    before((done) => {
+        done();
+    });
+
+    it("Unit test for BL", (done) => {
+        done();
+    });
+
+    it("Unit test for Model", (done) => {
+        done();
+    });
+
+    after((done) => {
+        done();
+    });
+
+});
