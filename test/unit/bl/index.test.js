012afe6203bdec386001fdafa0843e1052fdfc65
fadi <fadi@soajs.org>
2019-08-30 14:36:16 +0300

    Index BL init test case.

---
+++ test/unit/bl/index.test.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const coreModules = require("soajs.core.modules");
+const core = coreModules.core;
+const helper = require("../../helper.js");
+const BL = helper.requireModule('bl/index.js');
+const assert = require('assert');
+
+
+describe("Unit test for: BL - product", () => {
+
+    let soajs = {
+        config: {
+            "errors": {
+                460: "Unable to find product",
+                601: "Model not found"
+            },
+        },
+        log: {
+            error: () => {
+                console.log()
+            },
+            debug: () => {
+                console.log()
+            }
+        },
+        registry: {
+            get: () => {
+                return {
+                    "coreDB": {
+                        "provision": {
+                            "name": "core_provision",
+                            "prefix": "",
+                            "servers": [
+                                {
+                                    "host": "127.0.0.1",
+                                    "port": 27017
+                                }
+                            ],
+                            "credentials": null,
+                            "URLParam": {
+                                "poolSize": 5,
+                                "autoReconnect": true
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    describe("Unit test index init", () => {
+        it("Success - init", (done) => {
+            BL.init(soajs, soajs.config, (err, records) => {
+                console.log(err, records);
+                done();
+            });
+        });
+
+    });
+
+});
