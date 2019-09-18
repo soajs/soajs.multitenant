d9bf8e4a6375b8fe41e7024dbb3b45b32cecb005
fadi <fadi@soajs.org>
2019-09-18 14:18:52 +0300

    integration skeleton and schema for get and add tenant

--- test/integration/tenant/delete/deleteTenant.test.js
+++ test/integration/tenant/delete/deleteTenant.test.js
@@ -4,10 +4,10 @@ const requester = require('../../requester');
 
 let core = require('soajs').core;
 let validator = new core.validator.Validator();
-let addTenantSchema = require("../schemas/addTenant.js");
+let deleteTenantSchema = require("../schemas/deleteTenant.js");
 let getTenantsSchema = require("../schemas/getTenant.js");
 
-describe("Testing add tenant API", () => {
+describe("Testing delete tenant API", () => {
 
     before(function (done) {
         done();
@@ -18,14 +18,14 @@ describe("Testing add tenant API", () => {
         done();
     });
 
-    it("Success - will add tenant record - input", (done) => {
+    it("Success - will delete tenant record - input", (done) => {
         let params = {};
-        requester('/tenant', 'post', params, (error, body) => {
+        requester('/tenant', 'delete', params, (error, body) => {
             assert.ifError(error);
             assert.ok(body);
             assert.ok(body.data);
             assert.ok(body.data.length > 0);
-            let check = validator.validate(body, addTenantSchema);
+            let check = validator.validate(body, deleteTenantSchema);
             assert.deepEqual(check.valid, true);
             assert.deepEqual(check.errors, []);
             done();
@@ -39,7 +39,7 @@ describe("Testing add tenant API", () => {
             }
         };
         requester('/tenant', 'get', params, (error, body) => {
-            assert.ok(body);
+            assert.ok(error);
             let check = validator.validate(body, getTenantsSchema);
             assert.deepEqual(check.valid, true);
             assert.deepEqual(check.errors, []);
@@ -47,14 +47,13 @@ describe("Testing add tenant API", () => {
         });
     });
 
-
-    it("Fails - will not add tenant record - no input", (done) => {
+    it("Fails - will not delete tenant record - no input", (done) => {
         let params = {};
 
-        requester('/tenant', 'post', params, (error, body) => {
+        requester('/tenant', 'delete', params, (error, body) => {
             assert.ok(error);
 
-            let check = validator.validate(body, addTenantSchema);
+            let check = validator.validate(body, deleteTenantSchema);
             assert.deepEqual(check.valid, true);
             assert.deepEqual(check.errors, []);
             done();
