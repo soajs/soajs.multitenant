963133f71fddbc169cc6280f934766ff1fd18114
raghebad <ragheb.abo.dargham@gmail.com>
2019-10-23 21:14:03 +0300

    fixed expDate for tenant extkey in test cases

--- test/data/provision_integration/tenants/test.js
+++ test/data/provision_integration/tenants/test.js
@@ -33,7 +33,7 @@ let lib = {
 					"key": "d1eaaf5fdc35c11119330a8a0273fee9",
 					"extKeys": [
 						{
-							"expDate": new Date().getTime() + 7 * 24 * 3600 * 1000, // + 7 days
+							"expDate": null,
 							"extKey": "aa39b5490c4a4ed0e56d7ec1232a428f771e8bb83cfcee16de14f735d0f5da587d5968ec4f785e38570902fd24e0b522b46cb171872d1ea038e88328e7d973ff47d9392f72b2d49566209eb88eb60aed8534a965cf30072c39565bd8d72f68ac",
 							"dashboardAccess": true,
 							"device": {},
@@ -110,7 +110,7 @@ let lib = {
 					"key": "695d3456de70fddc9e1e60a6d85b97d3",
 					"extKeys": [
 						{
-							"expDate": new Date().getTime() + 86400000,
+							"expDate": null,
 							"extKey": "aa39b5490c4a4ed0e56d7ec1232a428f7ad78ebb7347db3fc9875cb10c2bce39bbf8aabacf9e00420afb580b15698c04ce10d659d1972ebc53e76b6bbae0c113bee1e23062800bc830e4c329ca913fefebd1f1222295cf2eb5486224044b4d0c",
 							"device": {},
 							"geo": {}
@@ -147,7 +147,7 @@ let lib = {
 					"key": "ff7b65bb252201121f1be95adc08f44a",
 					"extKeys": [
 						{
-							"expDate": new Date().getTime() + 86400000,
+							"expDate": null,
 							"extKey": "aa39b5490c4a4ed0e56d7ec1232a428f1c5b5dcabc0788ce563402e233386738fc3eb18234a486ce1667cf70bd0e8b08890a86126cf1aa8d38f84606d8a6346359a61678428343e01319e0b784bc7e2ca267bbaafccffcb6174206e8c83f2a25",
 							"device": {},
 							"geo": {}
--- test/data/provision_integration/tenants/test2.js
+++ test/data/provision_integration/tenants/test2.js
@@ -33,7 +33,7 @@ let lib = {
 					"key": "695d3456de70fddc9e1e60a6d85b97d3",
 					"extKeys": [
 						{
-							"expDate": new Date().getTime() + 86400000,
+							"expDate": null,
 							"extKey": "aa39b5490c4a4ed0e56d7ec1232a428f7ad78ebb7347db3fc9875cb10c2bce39bbf8aabacf9e00420afb580b15698c04ce10d659d1972ebc53e76b6bbae0c113bee1e23062800bc830e4c329ca913fefebd1f1222295cf2eb5486224044b4d0c",
 							"device": {},
 							"geo": {}
@@ -70,7 +70,7 @@ let lib = {
 					"key": "ff7b65bb252201121f1be95adc08f44a",
 					"extKeys": [
 						{
-							"expDate": new Date().getTime() + 86400000,
+							"expDate": null,
 							"extKey": "aa39b5490c4a4ed0e56d7ec1232a428f1c5b5dcabc0788ce563402e233386738fc3eb18234a486ce1667cf70bd0e8b08890a86126cf1aa8d38f84606d8a6346359a61678428343e01319e0b784bc7e2ca267bbaafccffcb6174206e8c83f2a25",
 							"device": {},
 							"geo": {}
